9427cc2150d4a31a751966ec782454acc8e47e0d
Chris-Totu <cris-hod@hotmail.com>
2023-02-15 12:29:58 +0100

    ok

--- exo_2_palindrome/Palindrome.py
+++ exo_2_palindrome/Palindrome.py
@@ -4,6 +4,6 @@ class Palindrome:
 
     def estPalindrome(self):  
         # Convertit la phrase en minuscules
-        phrase = self.phrase.lower() 
+        phrase = self.phrase.lower()
         # Vérifie si la phrase est égale à elle-même inversée
         return phrase == phrase[::-1]
