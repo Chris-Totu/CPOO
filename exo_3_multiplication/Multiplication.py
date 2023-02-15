04543aab4dd6c793f7356861b4438c0258097404
Chris-Totu <cris-hod@hotmail.com>
2023-02-15 12:26:25 +0100

    exo ok

---
+++ exo_3_multiplication/Multiplication.py
@@ -0,0 +1,9 @@
+class Multiplication:
+    def __init__(self, nbr):
+        self.nbr = nbr
+        self.const = nbr
+        
+    
+    def multiplie(self):
+        self.nbr *= self.const 
+        return self.nbr
---
+++ exo_3_multiplication/main.py
@@ -0,0 +1,6 @@
+from Multiplication import Multiplication
+
+multiplie = Multiplication(3)
+print(multiplie.multiplie())
+print(multiplie.multiplie())
+print(multiplie.multiplie())
