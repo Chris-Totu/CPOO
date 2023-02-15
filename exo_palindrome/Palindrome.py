947c871f9f07f41ce28117fb7a222c77106880a0
Chris_Totu <56915533+Chris-Totu@users.noreply.github.com>
2023-02-15 12:02:46 +0100

    Delete exo_palindrome directory

--- exo_palindrome/Palindrome.py
+++
@@ -1,9 +0,0 @@
-class Palindrome: 
-    def __init__(self, phrase):
-        self.phrase = phrase
-
-    def estPalindrome(self):  
-        # Convertit la phrase en minuscules et supprime les espaces
-        phrase = self.phrase.lower() 
-        # Vérifie si la phrase est égale à elle-même inversée et imprime True ou False
-        return phrase == phrase[::-1]
--- exo_palindrome/main.py
+++
@@ -1,7 +0,0 @@
-from Palindrome import Palindrome
-
-premier_palindrome = Palindrome('kayak')
-second_palindrome = Palindrome('ptdr')
-
-print(premier_palindrome.estPalindrome())
-print(second_palindrome.estPalindrome())
