5be99986d103f7b349abe702ed22899a45888aca
chris_totu <cris-hod@hotmail.com>
2023-02-17 20:44:44 +0100

    push end date

--- les_fichiers_python/exo_2/main.py
+++ les_fichiers_python/exo_2/main.py
@@ -5,3 +5,25 @@
 # ces données (Si oui, on l’invite alors à encoder ses nouvelles données) ou supprimer ces données.
 
 import os
+
+def register():
+    person = {'first_name' : '', 'last_name' : '', 'profession' : ''}
+    person['first_name'] = input("Entrez votre nom : ")
+    person['last_name'] = input("Entrez votre prénom : ")
+    person['profession'] = input("Entrez votre profession : ")
+
+    return person
+
+def add_in_file(person):
+    
+
+
+def is_present_in_file(person):
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
