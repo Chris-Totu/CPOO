e8ad511bda14da811c2bf781feb4960bc36de3f5
Chris-Totu <cris-hod@hotmail.com>
2023-02-18 02:16:45 +0100

    data

--- les_fichiers_python/exo_1/main.py
+++ les_fichiers_python/exo_1/main.py
@@ -7,10 +7,10 @@ import os
 def main():
     current = os.getcwd()
     print(current)
-    with open(f'{current}/les_fichiers_python/exo_1/fichier_source.txt', 'r') as f:
+    with open(f'{current}/CPOO/les_fichiers_python/exo_1/fichier_source.txt', 'r') as f:
         saving = f.read()
 
-    with open(f'{current}/les_fichiers_python/exo_1/fichier_terminus.txt', 'w') as f:
+    with open(f'{current}/CPOO/les_fichiers_python/exo_1/fichier_terminus.txt', 'w') as f:
         f.write(saving)
     
 if __name__ == "__main__":
--- les_fichiers_python/exo_2/main.py
+++ les_fichiers_python/exo_2/main.py
@@ -5,25 +5,49 @@
 # ces données (Si oui, on l’invite alors à encoder ses nouvelles données) ou supprimer ces données.
 
 import os
+from ast import literal_eval #convertir une chaîne de caractères qui représente un objet Python, tel qu'un dictionnaire ou une liste, en l'objet Python correspondant. 
 
 def register():
+    ### Demande les datas de la personne et return un dictionnaire sous forme d'un string ###
     person = {'first_name' : '', 'last_name' : '', 'profession' : ''}
     person['first_name'] = input("Entrez votre nom : ")
     person['last_name'] = input("Entrez votre prénom : ")
     person['profession'] = input("Entrez votre profession : ")
 
-    return person
+    return str(person)
 
 def add_in_file(person):
+    ### Ajoute une ligne des datas d'une personne dans le fichiers data.txt  ###
+    current = os.getcwd()
+    current += "/CPOO/les_fichiers_python/exo_2/data.txt"
     
-
+    with open(current, 'a') as f:
+        f.write(person + '\n') # \n pour qu'à la prochaine écriture ca aille à la ligne
 
 def is_present_in_file(person):
+    isPresent = False
+    current = os.getcwd()
+    current += "/CPOO/les_fichiers_python/exo_2/data.txt"
+    person = literal_eval(person) #retourner le dictionnaire qui etait passé en string
+    
+    with open(current, 'r') as f:
+        for line in f:
+            person_data = literal_eval(line)
+            if (person_data['first_name'] == person['first_name'] and
+            person_data['last_name'] == person['last_name'] and
+            person_data['profession'] == person['profession']):
+                isPresent = True
+                break
 
+    return isPresent
 
+def change_date():
+    pass
 
 def main():
-    pass
+    person = register()
+    #add_in_file(person)
+    print(is_present_in_file(person))
 
 if __name__ == "__main__":
     main()
